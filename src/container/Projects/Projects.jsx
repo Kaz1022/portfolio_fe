a40091acd562a9ed37499bec14fd224ac92e3a5c
Kazuko Baynton <kazukobaynton@gmail.com>
2022-11-11 15:44:38 -0800

    add mobile links

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { AiFillEye, AiFillGithub } from 'react-icons/ai';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
 
-import { urlFor, client } from '../../client';
+import { urlFor, client } from "../../client";
 import "./Projects.scss";
 
 const Projects = () => {
-  
   const [projects, setProjects] = useState([]);
   const [filterProject, setFilterProject] = useState([]);
-  const [activeFilter, setActiveFilter] = useState('All');
+  const [activeFilter, setActiveFilter] = useState("All");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
   useEffect(() => {
@@ -28,24 +27,28 @@ const Projects = () => {
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if (item === 'All') {
+      if (item === "All") {
         setFilterProject(projects);
       } else {
-        setFilterProject(projects.filter((project) => project.tags.includes(item)));
+        setFilterProject(
+          projects.filter((project) => project.tags.includes(item))
+        );
       }
     }, 500);
   };
 
   return (
-    <section className='app__projects' id="projects">
-      <h2 className="head-text" >PROJECTS</h2>
+    <section className="app__projects" id="projects">
+      <h2 className="head-text">PROJECTS</h2>
 
       <div className="app__project-filter">
-        {['React', 'Javascript', 'Fullstack', 'All'].map((item, index) => (
+        {["React", "Javascript", "Fullstack", "All"].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
-            className={`app__project-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+            className={`app__project-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
           >
             {item}
           </div>
@@ -59,31 +62,33 @@ const Projects = () => {
       >
         {filterProject.map((work, index) => (
           <div className="app__project-item app__flex" key={index}>
-            <div
-              className="app__project-img app__flex"
-            >
+            <div className="app__project-img app__flex">
               <img src={urlFor(work.imgUrl)} alt={work.name} />
-                <motion.div
+              <motion.div
                 whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
                 className="app__project-hover app__flex"
               >
-              {work.projectLink && 
-                <a href={work.projectLink} target="_blank" rel="noreferrer">              
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillEye />
-                  </motion.div>
-                </a>
-              }
+                {work.projectLink && (
+                  <a href={work.projectLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                )}
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -94,8 +99,35 @@ const Projects = () => {
             </div>
 
             <div className="app__project-content app__flex">
+              {/* mobile link for github and live */}
+              <div className="app__project-mobilelink app__flex">
+              {work.projectLink && (
+                  <a href={work.projectLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1.5] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                )}
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1.5] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </div>
               <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
 
               <div className="app__project-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
